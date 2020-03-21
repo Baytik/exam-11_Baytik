4275894008372ffd65b05bb08d194f9a0d13f658
Baytik Tumanbaev <jumka.wows@gmail.com>
2020-03-21 12:09:44 +0600

    Сделал регистрацию и вход в backend

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const users = require('./app/users');
+/*const posts = require('./app/posts');
+const comments = require('./app/comments');*/
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+const run = async () => {
+    await mongoose.connect('mongodb://localhost/posts', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
+    app.use('/users', users);
+/*    app.use('/posts', posts);
+    app.use('/comments', comments);*/
+    app.listen(port)
+};
+
+run().catch(e => {
+    console.error(e)
+});
